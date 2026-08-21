58e9a01460d6c91319ec6ac21bfc0b0f52635a32
tmcausla <ta_mcausland@hotmail.com>
2026-08-21 18:35:41 -0400

    Implement recursive static -> public file copying

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-from textnode import TextNode, TextType
+import shutil
+import os
 
-def main():
-    test1 = TextNode("This is some anchor text", TextType.LINK, "https://example.com")
-    test2 = TextNode("This will be plain text", TextType.TEXT)
-    test3 = TextNode("An image of magi", TextType.IMAGE, "www.magiimages.we")
-    print(test1)
-    print(test2)
-    print(test3)
+def static_to_public(static_path='static', public_path='public'):
+    if os.path.exists(static_path):
+        for entry in os.listdir(static_path):
+            new_static_path = os.path.join(static_path, entry)
+            new_public_path = os.path.join(public_path, entry)
+    
+            if os.path.isdir(new_static_path):
+                os.mkdir(new_public_path)
+                static_to_public(new_static_path, new_public_path)
 
+            elif os.path.isfile(new_static_path):
+                shutil.copy(new_static_path, new_public_path)
+                print(f"created path: {new_public_path}")
+
+def main(): #this should probably be in a separate script
+    if os.path.exists('public'):
+        shutil.rmtree('public')
+        print('public was cleared')
+    os.mkdir('public')
+    print('made public dir')
+
+    static_to_public()
 main()
